d07b2e99ec60ed0408c387cc1c276aeb96c64383
sejoung kim <sejoung@gmail.com>
2025-02-27 20:54:55 +0900

    redis 컨테이너 설정

--- tests/test_produce.py
+++ tests/test_produce.py
@@ -5,6 +5,7 @@ from sejoung.producer.produce import produce
 
 def test_produce(redis_container_url: str):
     produce(redis_container_url)
-    client = redis.from_url(redis_container_url)
-    actual = client.llen("rq:queue:default")
+    with redis.from_url(redis_container_url) as client:
+        actual = client.llen("rq:queue:default")
+
     assert actual == 1
